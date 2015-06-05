1c2a1e5636a452e55f535b4eb04e7f1aad35e15c
James Boocock <james.boocock@otago.ac.nz>
2015-06-05 16:26:31 +1200

    Deleted python build data

--- build/lib/genomic_operations/operations/output.py
+++
@@ -1,8 +0,0 @@
-def print_result(temp_result, output):
-    """
-        Writes a SNPposition file to stdoutput
-    """
-    output.write(str(temp_result) + '\n')
-    for line in temp_result:
-        output.write(str(line) + '\n')
-
